fa7609da4f8f2fb385ad7133f1cb64f94aeef5d9
Albot Goodman <albot.goodman@gmail.com>
2026-01-25 20:09:11 +0100

    refactor: improved the example pipeline

--- src/minigrid/env_optimiser.py
+++ src/minigrid/env_optimiser.py
@@ -5,7 +5,7 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import SubprocVecEnv, VecEnv, VecMonitor
 
 from minigrid.minigrid_env import MiniGridEnv
-from minigrid.wrappers import ImgObsWrapper, Wrapper
+from minigrid.wrappers import Wrapper
 
 
 class EnvOptimiser:
@@ -44,9 +44,9 @@ class EnvOptimiser:
         """Make vectorized environment."""
         self.log_dir.mkdir(parents=True, exist_ok=True)
         vec_env = self.vec_env_cls([self._build_env(i) for i in range(self.n_envs)])
-        return VecMonitor(
-            vec_env, str(self.log_dir)
-        )  # record episode reward, length, time to csv
+
+        # Record episode reward, length, time to csv
+        return VecMonitor(vec_env, str(self.log_dir))
 
 
 def test_optimiser() -> None:
@@ -54,10 +54,18 @@ def test_optimiser() -> None:
     from feature_extractor import MinigridFeaturesExtractor
     from simple_env import SimpleEnv
 
+    from minigrid.wrappers import ImgObsWrapper
+
+    n_envs = 8
+    n_timesteps = 5_000
+
+    log_dir = Path("logs/")
+    log_dir.mkdir(parents=True, exist_ok=True)
+    save_dir = Path("models/")
+    save_dir.mkdir(parents=True, exist_ok=True)
+
     optimiser = EnvOptimiser(
-        env_cls=SimpleEnv,
-        n_envs=4,
-        wrapper_cls=[ImgObsWrapper],
+        env_cls=SimpleEnv, n_envs=n_envs, wrapper_cls=[ImgObsWrapper], log_dir=log_dir
     )
     vec_env_train = optimiser.build_vec_env()
     policy_kwargs = {
@@ -65,8 +73,8 @@ def test_optimiser() -> None:
         "features_extractor_kwargs": {"features_dim": 128},
     }
     model = PPO(policy="CnnPolicy", env=vec_env_train, policy_kwargs=policy_kwargs)
-    model.learn(total_timesteps=20_000, progress_bar=True)
-    model.save("test_model")
+    model.learn(total_timesteps=n_timesteps, progress_bar=True)
+    model.save(save_dir / "test_model")
 
 
 if __name__ == "__main__":
